5a333bd0960dbf7bd9b32c5599262bf9180aa854
donggook-me <ert1306@naver.com>
2022-09-11 21:09:45 +0900

    rename

--- backjoon_1009.java
+++ backjoon_1009.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.lang.Math;
 
 
-public class Main {
+public class backjoon_1009 {
     public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
