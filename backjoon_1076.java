e2fdcb15cfc665499daa943d6abc4bcd9b7ba473
donggook-me <ert1306@naver.com>
2022-09-14 00:43:08 +0900

    220913

---
+++ backjoon_1076.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class backjoon_1076 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> color_list = new ArrayList<>(Arrays.asList("black", "brown", "red", "orange","yellow","green","blue","violet","grey","white"));
+
+
+        String first_color = br.readLine();
+        String second_color = br.readLine();
+        String third_color = br.readLine();
+
+        String first_value = String.valueOf(color_list.indexOf(first_color));
+        String second_value = String.valueOf(color_list.indexOf(second_color));
+        long combined_value = Long.parseLong(first_value + second_value + ("0".repeat(color_list.indexOf(third_color) )));
+        System.out.println(combined_value);
+
+//        System.out.println((int) Math.pow(10, color_list.indexOf(third_color)));
+//        int result = combined_value * (int) Math.pow(10, color_list.indexOf(third_color));
+//
+//        System.out.println(result);
+
+    }}
